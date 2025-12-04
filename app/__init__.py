f74bb182417fd08d9e232648aca9d9f4064f66cf
MUZAAQI <muzaaqi@yahoo.com>
2025-12-04 12:29:20 +0700

    refactor(init): Refactor Flask app initialization and add extensions
    Refactored app initialization to use a factory function. Added CORS and JWTManager support, and updated SQLAlchemy and Migrate initialization. Registered API blueprint for improved modularity.

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,27 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
-app = Flask(__name__)
-app.config.from_object(Config)
+db = SQLAlchemy()
+migrate = Migrate()
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-from app.models import User, Todo, Category
-from app import routes
+    db.init_app(app)
+    migrate.init_app(app, db)
+    CORS(app, resources={r"/api/*": {"origins": ["http://localhost:3000", "http://localhost:5173", "https://todo.muzaaqi.my.id"]}})
+    JWTManager(app)
+
+    from app.models import User
+    from app.models import Todo
+    from app.models import Category
+    from app.models import todo_categories
+
+    from app.routes import api
+    app.register_blueprint(api)
+
+    return app
